b7f8e744f7f6c7ea040fe33c83436543862b883e
gabrielcorado <gabrielcorado@mail.com>
2016-09-29 17:00:33 -0300

    Correct error

--- web/src/store/mock.js
+++ web/src/store/mock.js
@@ -1,7 +1,6 @@
 //
 export default {
-  0: 'Oi, eu sou o viajinho, o seu amiguinho de viagens, meu objetivo é achar
-    o melhor agente de viajens para você, vamos começar pelo seu nome, como eu posso te chamar?',
+  0: 'Oi, eu sou o viajinho, o seu amiguinho de viagens, meu objetivo é achar o melhor agente de viajens para você, vamos começar pelo seu nome, como eu posso te chamar?',
   1: 'Voce geralmente viaja?',
   2: 'Qual experiencia voce esta procurando?',
   default: 'Ok...'
